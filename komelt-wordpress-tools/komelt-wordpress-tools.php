7d5d3a3dfbd6fd2ba428603ea8c2cc1e5a4a5bed
Tilen Komel <tilen.komel10@gmail.com>
2022-03-08 10:34:20 +0100

    Initial Commit

---
+++ komelt-wordpress-tools/komelt-wordpress-tools.php
@@ -0,0 +1,21 @@
+/**
+ * Komelt WordPress Tools
+ *
+ * @package           komelt-wordpress-tools
+ * @author            Tilen Komel
+ * @copyright         2022 Komelt.dev
+ * @license           GPL-2.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Komelt WordPress Tools
+ * Plugin URI:        https://github.com/KomelT/komelt-wordpress-tools
+ * Description:       Plugin with simple tools.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tilen Komel
+ * Author URI:        https://www.komelt.dev
+ * Text Domain:       komelt-wordpress-tools
+ * License:           GPL v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
